b6f2e56f96f1593a5e7c7ddb50fbe66ebdfd7ed5
Linards Kalvans <linards.kalvans@tieto.com>
2017-11-02 13:47:45 +0200

    Add logging and row_factory

--- ObsDataRest.py
+++ ObsDataRest.py
@@ -2,21 +2,39 @@ from db import conn, cursor
 
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+import logging
+
+logging.basicConfig(
+    filename = '%s.log' % __name__,
+    level=logging.DEBUG,
+    format='%(asctime)s %(message)s',
+)
 
 app = Flask(__name__)
 api = Api(app)
 
 class  DataSources(Resource):
     def get(self):
-        query = cursor.execute('select * from DataSources')
-        return{'DataSources':  [i[0] for i in query.fetchall()]}
+        logging.info("%s.%s()" % (self.__class__.__name__, "get"))
+        try:
+            query = cursor.execute('select * from DataSources')
+            rv = query.fetchall()
+            logging.info("%s.%s() = %s" % (self.__class__.__name__, "get", rv))
+            return{'DataSources': rv}
+        except:
+            logging.error("%s.%s() failed" % (self.__class__.__name__, "get"), exc_info = True)
+            raise
 
     def post(self):
-        print(request.json)
-        id = request.json['DataSourceID']
-        name = request.json['Name']
-        desc = request.json['Description']
-        cursor.execute('insert into DataSources values (?,?,?)',(id, name, desc))
-        conn.commit()
-        
-api.add_resource(DataSources, '/sources') 
+        logging.info("%s.%s(%s)" % (self.__class__.__name__, "post", request.json))
+        try:
+            id = request.json['DataSourceID']
+            name = request.json['Name']
+            desc = request.json['Description']
+            cursor.execute('insert into DataSources values (?,?,?)',(id, name, desc))
+            conn.commit()
+        except:
+            logging.error("%s.%s() failed" % (self.__class__.__name__, "post"), exc_info = True)
+            raise
+
+api.add_resource(DataSources, '/sources')
--- db.py
+++ db.py
@@ -5,5 +5,13 @@ config = configparser.ConfigParser()
 config.read("ObsDataRest.cfg")
 
 db_file = config["database"]["path"]
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
 conn = sqlite3.connect(db_file)
+conn.row_factory = dict_factory
 cursor = conn.cursor()
