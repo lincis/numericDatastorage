a6e79ac826d04e5b69e51c0e4b1305372a462d20
Linards Kalvans <linards.kalvans@gmail.com>
2017-11-05 21:57:54 +0200

    Better return codes

--- ObsDataRest/ObsDataRest.py
+++ ObsDataRest/ObsDataRest.py
@@ -11,13 +11,13 @@ api = Api(app)
 
 mypath = os.path.dirname(os.path.realpath(__file__))
 config = configparser.ConfigParser()
-config.read(os.path.join(mypath,"ObsDataRest.cfg"))
+config.read(os.path.join(mypath,'ObsDataRest.cfg'))
 
-db_file = os.path.join(mypath,config["database"]["path"])
+db_file = os.path.join(mypath,config['database']['path'])
 
 def init_db(path = None):
     if not path:
-        path = getattr(g, "db_file", db_file)
+        path = getattr(g, 'db_file', db_file)
     with app.app_context():
         conn, cursor = get_conn(path, True)
         with app.open_resource('struct.sql', mode='r') as f:
@@ -33,12 +33,19 @@ logging.basicConfig(
 
 class _ODRBase(Resource):
     def __init__(self):
-        self.conn, self.cursor = get_conn(getattr(g, "db_file", db_file))
+        self.conn, self.cursor = get_conn(getattr(g, 'db_file', db_file))
         super(_ODRBase, self).__init__()
+
+    def _check_entry(self,id):
+        table = self.__class__.__name__
+        id_name = table[:-1] + 'ID'
+        query = 'select %s from %s where %s = ?' % (id_name, table, id_name)
+        logging.debug('%s.%s(%s) query = %s' % (self.__class__.__name__, '_check_entry', id, query))
+        return bool(self.cursor.execute(query, (id,)).fetchall())
 
 class  DataSources(_ODRBase):
     def get(self, source_id = None):
-        logging.info("%s.%s(%s)" % (self.__class__.__name__, "get", source_id))
+        logging.info('%s.%s(%s)' % (self.__class__.__name__, 'get', source_id))
         try:
             if source_id:
                 query = self.cursor.execute('select * from DataSources where DataSourceID = ?', (source_id,))
@@ -46,35 +53,46 @@ class  DataSources(_ODRBase):
             else:
                 query = self.cursor.execute('select * from DataSources')
                 rv = query.fetchall()
-            logging.info("%s.%s() = %s" % (self.__class__.__name__, "get", rv))
-            return{'DataSources': rv}
+            logging.info('%s.%s() = %s' % (self.__class__.__name__, 'get', rv))
         except:
-            logging.error("%s.%s() failed" % (self.__class__.__name__, "get"), exc_info = True)
+            logging.error('%s.%s() failed' % (self.__class__.__name__, 'get'), exc_info = True)
             raise
+        if rv:
+            return{'DataSources': rv}
+        else:
+            return '',404
 
-    def post(self, source_id):
-        logging.info("%s.%s(%s, %s)" % (self.__class__.__name__, "post", source_id, request.json))
+    def put(self, source_id):
+        logging.info('%s.%s(%s, %s)' % (self.__class__.__name__, 'post', source_id, request.json))
+        if not source_id:
+            return 'Please specify DataTypeID', 405
         try:
             name = request.json['Name']
             desc = request.json['Description']
-            self.cursor.execute('insert into DataSources values (?,?,?)',(source_id, name, desc))
+            if self._check_entry(source_id):
+                self.cursor.execute('update DataSources set Name = ?, Description = ? where DataSourceID = ?',(name, desc, source_id))
+                rc = 201
+            else:
+                self.cursor.execute('insert into DataSources values (?,?,?)',(source_id, name, desc))
+                rc = 200
             self.conn.commit()
         except:
-            logging.error("%s.%s() failed" % (self.__class__.__name__, "post"), exc_info = True)
+            logging.error('%s.%s() failed' % (self.__class__.__name__, 'post'), exc_info = True)
             raise
+        return '',rc
 
     def delete(self, source_id):
-        logging.info("%s.%s(%s)" % (self.__class__.__name__, "delete", source_id))
+        logging.info('%s.%s(%s)' % (self.__class__.__name__, 'delete', source_id))
         try:
             self.cursor.execute('delete from DataSources where DataSourceID = ?',(source_id,))
             self.conn.commit()
         except:
-            logging.error("%s.%s() failed" % (self.__class__.__name__, "delete"), exc_info = True)
+            logging.error('%s.%s() failed' % (self.__class__.__name__, 'delete'), exc_info = True)
             raise
 
 class DataTypes(_ODRBase):
     def get(self, type_id = None):
-        logging.info("%s.%s(%s)" % (self.__class__.__name__, "get", type_id))
+        logging.info('%s.%s(%s)' % (self.__class__.__name__, 'get', type_id))
         try:
             if type_id:
                 query = self.cursor.execute('select * from DataTypes where DataTypeID = ?', (type_id,))
@@ -82,22 +100,24 @@ class DataTypes(_ODRBase):
             else:
                 query = self.cursor.execute('select * from DataTypes')
                 rv = query.fetchall()
-            logging.info("%s.%s() = %s" % (self.__class__.__name__, "get", rv))
-            return{'DataTypes': rv}
+            logging.info('%s.%s() = %s' % (self.__class__.__name__, 'get', rv))
         except:
-            logging.error("%s.%s() failed" % (self.__class__.__name__, "get"), exc_info = True)
+            logging.error('%s.%s() failed' % (self.__class__.__name__, 'get'), exc_info = True)
             raise
+        if rv:
+            return{'DataTypes': rv}
+        else:
+            return '',404
 
     def put(self, type_id = None):
-        logging.info("%s.%s(%s, %s)" % (self.__class__.__name__, "put", type_id, request.json))
+        logging.info('%s.%s(%s, %s)' % (self.__class__.__name__, 'put', type_id, request.json))
         if not type_id:
-            return '', 405
+            return 'Please specify DataTypeID', 405
         try:
             name = request.json['Name']
             desc = request.json['Description']
             units = request.json['Units']
-            query = self.cursor.execute('select DataTypeID from DataTypes where DataTypeID = ?', (type_id,))
-            if len(query.fetchall()):
+            if self._check_entry(type_id):
                 self.cursor.execute('update DataTypes set Name = ?, Description = ?, Units = ? where DataTypeID = ?',(name, desc, units, type_id))
                 rc = 201
             else:
@@ -105,17 +125,19 @@ class DataTypes(_ODRBase):
                 rc = 200
             self.conn.commit()
         except:
-            logging.error("%s.%s() failed" % (self.__class__.__name__, "put"), exc_info = True)
+            logging.error('%s.%s() failed' % (self.__class__.__name__, 'put'), exc_info = True)
             raise
         return request.json, rc
 
     def delete(self, type_id):
-        logging.info("%s.%s(%s)" % (self.__class__.__name__, "delete", type_id))
+        logging.info('%s.%s(%s)' % (self.__class__.__name__, 'delete', type_id))
+        if not self._check_entry(type_id):
+            return '', 404
         try:
             self.cursor.execute('delete from DataTypes where DataTypeID = ?',(type_id,))
             self.conn.commit()
         except:
-            logging.error("%s.%s() failed" % (self.__class__.__name__, "delete"), exc_info = True)
+            logging.error('%s.%s() failed' % (self.__class__.__name__, 'delete'), exc_info = True)
             raise
 
 api.add_resource(DataSources,
--- test_api.py
+++ test_api.py
@@ -10,7 +10,7 @@ type_id = str(uuid.uuid4())
 class TestDataSources:
     def setup_class(self):
         with app.app_context():
-            init_db("./.test.db")
+            init_db('./.test.db')
         app.testing = True
         self.client = app.test_client()
 
@@ -21,40 +21,33 @@ class TestDataSources:
         assert r.status_code == 200
 
     @pytest.mark.first
-    def test_source_post(self):
-        r = self.client.post('/sources/%s' % (source_id),
+    @pytest.mark.parametrize('code',(200,201))
+    def test_source_post(self, code):
+        r = self.client.put('/sources/%s' % (source_id),
             data=json.dumps({ 'Name': 'Test', 'Description': 'Test description' }),
             content_type='application/json')
-        assert r.status_code == 200
+        assert r.status_code == code
 
-    def test_source_post2(self):
-        with pytest.raises(sqlite3.IntegrityError):
-            r = self.client.post('/sources/%s' % (source_id),
-                data=json.dumps({ 'Name': 'Test', 'Description': 'Test description' }),
-                content_type='application/json')
-
-    @pytest.mark.parametrize('id',('',source_id))
+    @pytest.mark.parametrize('id',('',source_id, 'False'))
     def test_source_get(self, id):
         r = self.client.get('/sources/%s' % (id),
             content_type='application/json')
-        assert r.status_code == 200
-        data = r_data = json.loads(r.data)["DataSources"]
+        if id == 'False':
+            assert r.status_code == 404
+            return
+        data = r_data = json.loads(r.data)['DataSources']
         if not id:
             r_data = data.pop()
-        assert r_data["DataSourceID"] == source_id
+        assert r.status_code == 200
+        assert r_data['DataSourceID'] == source_id
 
     @pytest.mark.first
-    def test_type_post(self):
+    @pytest.mark.parametrize('code',(200,201))
+    def test_type_post(self, code):
         r = self.client.put('/types/%s' % (type_id),
             data=json.dumps({ 'Name': 'Test', 'Description': 'Test description', 'Units': 'Test units' }),
             content_type='application/json')
-        assert r.status_code == 200
-
-    def test_type_post2(self):
-        r = self.client.put('/types/%s' % (type_id),
-            data=json.dumps({ 'Name': 'Test', 'Description': 'Test description', 'Units': 'Test units' }),
-            content_type='application/json')
-        assert r.status_code == 201
+        assert r.status_code == code
 
     def test_type_post3(self):
         r = self.client.put('/types/',
@@ -62,12 +55,15 @@ class TestDataSources:
             content_type='application/json')
         assert r.status_code == 405
 
-    @pytest.mark.parametrize('id',('',type_id))
+    @pytest.mark.parametrize('id',('',type_id, 'False'))
     def test_type_get(self, id):
         r = self.client.get('/types/%s' % (id),
             content_type='application/json')
-        assert r.status_code == 200
-        data = r_data = json.loads(r.data)["DataTypes"]
+        if id == 'False':
+            assert r.status_code == 404
+            return
+        data = r_data = json.loads(r.data)['DataTypes']
         if not id:
             r_data = data.pop()
-        assert r_data["DataTypeID"] == type_id
+        assert r.status_code == 200
+        assert r_data['DataTypeID'] == type_id
