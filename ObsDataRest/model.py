7e5c2b401e23d3911ed7148914db855ffcd11198
Linards Kalvāns <linards.kalvans@twino.eu>
2019-11-11 15:49:29 +0200

    Add passowrd hashing

--- ObsDataRest/model.py
+++ ObsDataRest/model.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import app
 
 db = SQLAlchemy(app)
@@ -11,6 +12,13 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
 
 class DataTypes(db.Model):
     id = db.Column(db.String(36), primary_key = True, nullable = False)
