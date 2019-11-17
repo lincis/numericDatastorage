033193be15427a2f069bdbfbf821e5a942706c89
Linards Kalvāns <linards.kalvans@gmail.com>
2019-11-18 00:26:52 +0200

    Add DB creation script

--- create_db.py
+++ create_db.py
@@ -1,5 +1,6 @@
 from ObsDataRest import app, db, UserModel, DataModel, DataTypesModel, DataSourcesModel
 from sqlalchemy.exc import IntegrityError
+import os
 
 with app.app_context():
     db.create_all()
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from ObsDataRest import app, db, UserModel, DataModel, DataTypesModel, DataSourcesModel
-import os
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0')
