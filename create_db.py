6972f5ee015bc2c191bd405d2cbb8419f304dc22
Linards Kalvāns <linards.kalvans@gmail.com>
2019-11-18 00:16:58 +0200

    Add DB creation script

---
+++ create_db.py
@@ -0,0 +1,11 @@
+from ObsDataRest import app, db
+
+with app.app_context():
+    db.create_all()
+    try:
+        user = UserModel(username = os.environ.get('API_USER'))
+        user.set_password(os.environ.get('API_PW'))
+        user.insert()
+    except IntegrityError:
+        print("Default user already exists")
+        pass
--- main.py
+++ main.py
@@ -3,13 +3,4 @@ from sqlalchemy.exc import IntegrityError
 import os
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        try:
-            user = UserModel(username = os.environ.get('API_USER'))
-            user.set_password(os.environ.get('API_PW'))
-            user.insert()
-        except IntegrityError:
-            print("Default user already exists")
-            pass
-        app.run(host = '0.0.0.0')
+    app.run(host = '0.0.0.0')
