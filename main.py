ba58b191702459cae2e68ff110bcfb6af702beb2
Linards Kalvāns <linards.kalvans@twino.eu>
2019-12-23 21:32:45 +0200

    Create DB before running flask

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from gevent import monkey
 monkey.patch_all()
 
-from ObsDataRest import app, socketio
+from ObsDataRest import app, socketio, db, UserModel, DataModel, DataTypesModel, DataSourcesModel
+from sqlalchemy.exc import IntegrityError
+import os
+
+with app.app_context():
+    db.create_all()
+    try:
+        user = UserModel(username = os.environ.get('API_USER'))
+        user.set_password(os.environ.get('API_PW'))
+        user.insert()
+    except IntegrityError:
+        print("Default user already exists")
+        pass
 
 if __name__ == '__main__':
     socketio.run(app, debug = True, host = '0.0.0.0')
