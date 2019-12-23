34bc1213d34ebb38f98be82e6107f74fec0978fd
Linards Kalvāns <linards.kalvans@twino.eu>
2019-12-23 21:36:39 +0200

    Run redis from main.py

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM python:latest
 RUN uname -a
 RUN apt-get update
 RUN apt-get -y --no-install-recommends install libpq-dev
-RUN apt-get -y --no-install-recommends install redis-server systemd
-RUN systemctl enable redis-server
+RUN apt-get -y --no-install-recommends install redis-server
 COPY . /app
 RUN pip install -r /app/requirements.txt
--- main.py
+++ main.py
@@ -4,16 +4,20 @@ monkey.patch_all()
 from ObsDataRest import app, socketio, db, UserModel, DataModel, DataTypesModel, DataSourcesModel
 from sqlalchemy.exc import IntegrityError
 import os
-
-with app.app_context():
-    db.create_all()
-    try:
-        user = UserModel(username = os.environ.get('API_USER'))
-        user.set_password(os.environ.get('API_PW'))
-        user.insert()
-    except IntegrityError:
-        print("Default user already exists")
-        pass
+import subprocess
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        try:
+            user = UserModel(username = os.environ.get('API_USER'))
+            user.set_password(os.environ.get('API_PW'))
+            user.insert()
+        except IntegrityError:
+            print("Default user already exists")
+            pass
+    try:
+        subprocess.run(["service redis-server start"])
+    except:
+        print("Could not start redis")
     socketio.run(app, debug = True, host = '0.0.0.0')
